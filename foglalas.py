ba047a85093a96c7b4d660004e86e6a35a1a9893
Tsury0 <7ns.verebes@GMAIL.COM>
2022-12-01 18:07:47 +0100

    terv

---
+++ foglalas.py
@@ -0,0 +1,12 @@
+# Budapest - Debrecen (egyirányú)
+# Budapest - Bécs (egyirányú)
+# Budapest - Szeged (egyirányú)
+
+# 1 nagy kocsi, 12asztal, 1asztal 4fő = 48férőhley
+
+#jegyar = 4000
+
+#! Választás
+#! Kiindulási pont: Budapest
+#! Cél: Bécs, Debrecen, Szeged
+#! Időpont: Reggel 06:00, délben 12:00, este 18:00
